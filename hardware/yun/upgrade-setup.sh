6288d1a32304c1127fa108e2ff88bdf675d674c7
Travis Ard <travis.ard@gmail.com>
2014-11-21 21:00:02 +0000

    Made sure the SD card was present before continuing

--- hardware/yun/upgrade-setup.sh
+++ hardware/yun/upgrade-setup.sh
@@ -1,15 +1,25 @@
 echo "Upgrading the Yun system. Please wait..."
 
-# install unzip utility
-opkg update
-opkg install unzip
+if [ ! -d /mnt/sda1 ]; then
 
-# download latest openwrt image
-wget http://arduino.cc/download.php?f=/openwrtyun/1/YunSysupgradeImage_v1.5.3.zip -O YunSysupgradeImage_v1.5.3.zip
+	echo "MicroSD is not available. Please re-insert, or format as FAT32"
 
-# unzip image
-unzip YunSysupgradeImage_v1.5.3.zip
-rm YunSysupgradeImage_v1.5.3.zip
+else
 
-# update system
-run-sysupgrade openwrt-ar71xx-generic-yun-16M-squashfs-sysupgrade.bin
+    cd /mnt/sda1
+
+    # install unzip utility
+    opkg update
+    opkg install unzip
+    
+    # download latest openwrt image
+    wget http://arduino.cc/download.php?f=/openwrtyun/1/YunSysupgradeImage_v1.5.3.zip -O YunSysupgradeImage_v1.5.3.zip
+    
+    # unzip image
+    unzip YunSysupgradeImage_v1.5.3.zip
+    rm YunSysupgradeImage_v1.5.3.zip
+    
+    # update system
+    run-sysupgrade openwrt-ar71xx-generic-yun-16M-squashfs-sysupgrade.bin
+    
+fi
