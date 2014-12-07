b4ba4c4c8efe69c3bd5ae5846675944f01347e5b
Travis Ard <travis.ard@gmail.com>
2014-12-07 03:19:59 +0000

    cors

--- omni.js
+++ omni.js
@@ -608,7 +608,23 @@ var httpServer = http.createServer(function (req, res) {
 
 	}
 
-	if (moduleName === 'ui') {
+	if (req.method === 'OPTIONS') {
+
+	      var headers = {};
+
+	      // IE8 does not allow domains to be specified, just the *
+	      // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+	      headers["Access-Control-Allow-Origin"] = "*";
+	      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+	      headers["Access-Control-Allow-Credentials"] = false;
+	      headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+	      headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+
+	      res.writeHead(200, headers);
+	      return res.end();
+	}
+	
+	else if (moduleName === 'ui') {
 		
 		if (req.method === 'GET') {
 		
